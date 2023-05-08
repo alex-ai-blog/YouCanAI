eae1c29800530ac8080976474257e64555539140
alex-ai-blog <131699635+alex-ai-blog@users.noreply.github.com>
2023-05-08 15:40:10 -0400

    Add files via upload

--- YouCanAI/2023-05-09 Leetcode/01-roman2integer.py
+++ YouCanAI/2023-05-09 Leetcode/01-roman2integer.py
@@ -1,12 +1,28 @@
+
+# Prompt:
+# Complete the following code in python3. It should be a function to convert roman numerals to integers. It should work on roman numerals up to M. 
+
+# class Solution: 
+
+#     def romanToInt(self, s: str) -> int: 
+
+
 # Accepted
 
 class Solution:
+
     def romanToInt(self, s: str) -> int:
-        roman_to_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        
+        values = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
         result = 0
-        for i in range(len(s)):
-            if i > 0 and roman_to_int[s[i]] > roman_to_int[s[i - 1]]:
-                result += roman_to_int[s[i]] - 2 * roman_to_int[s[i - 1]]
+        i = 0
+        
+        while i < len(s):
+            if i+1 < len(s) and values[s[i]] < values[s[i+1]]:
+                result += values[s[i+1]] - values[s[i]]
+                i += 2
             else:
-                result += roman_to_int[s[i]]
+                result += values[s[i]]
+                i += 1
+                
         return result
